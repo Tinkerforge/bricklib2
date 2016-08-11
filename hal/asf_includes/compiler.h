ebba6f882f77f288f5ceb6933e5695a7f992252f
Olaf Lüke <olaf@tinkerforge.com>
2016-08-11 14:35:44 +0200

    Fix bug in compiler.h

--- hal/asf_includes/compiler.h
+++ hal/asf_includes/compiler.h
@@ -1,5 +1,5 @@
 #if defined(__SAM0__)
 #include "bricklib2/asf/sam0/utils/compiler.h"
 #elif defined(__SAM__)
-#include "bricklib2/asf/sam/utils/compiler.h"
+#include "bricklib2/asf_overwrite/sam/utils/compiler.h"
 #endif

37fdcefac2c00aaf7c3c89d9e2c935311eddab76
Olaf Lüke <olaf@tinkerforge.com>
2016-08-23 16:01:16 +0200

    Use sercom spi for sam0

--- hal/asf_includes/spi.h
+++ hal/asf_includes/spi.h
@@ -1,5 +1,5 @@
 #if defined(__SAM0__)
-#include "bricklib2/asf/sam0/drivers/spi/spi.h"
+#include "bricklib2/asf/sam0/drivers/sercom/spi/spi.h"
 #elif defined(__SAM__)
 #include "bricklib2/asf/sam/drivers/spi/spi.h"
 #endif

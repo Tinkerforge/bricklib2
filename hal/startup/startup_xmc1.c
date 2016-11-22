eec056e73ffbca201bbd43f22f2a96c497b83c70
Olaf Lüke <olaf@tinkerforge.com>
2016-11-22 15:56:04 +0100

    Add support for external clock with internal clock calibration

--- hal/startup/startup_xmc1.c
+++ hal/startup/startup_xmc1.c
@@ -22,6 +22,7 @@
 
 #include "configs/config.h"
 #include "xmc_device.h"
+#include "xmc_scu.h"
 
 #ifdef STARTUP_SYSTEM_INIT_ALREADY_DONE
 #include "bricklib2/bootloader/bootloader.h"
@@ -32,29 +33,56 @@
 #define DCO1_FREQUENCY 64000000U
 #elif UC_SERIES == XMC14
 
-#define DCO1_FREQUENCY  48000000U
-#define OSCHP_FREQUENCY 20000000U // External crystal frequency [Hz]
+#define DCO1_FREQUENCY  48000000
+#ifndef OSCHP_FREQUENCY
+#define OSCHP_FREQUENCY 16000000 // External crystal frequency [Hz]
+#endif
 
 // DCLK clock source selection: "Internal oscillator DCO1 (48MHz)" or "External crystal oscillator"
 #define DCLK_CLOCK_SRC_DCO1 0
 #define DCLK_CLOCK_SRC_EXT_XTAL 1
+#ifndef DCLK_CLOCK_SRC
 #define DCLK_CLOCK_SRC DCLK_CLOCK_SRC_DCO1
+#endif
 
 // SCHP external oscillator mode: "Crystal mode" or "External clock direct input mode"
 #define OSCHP_MODE_XTAL 0
 #define OSCHP_MODE_DIRECT 1
+#ifndef OSCHP_MODE
 #define OSCHP_MODE OSCHP_MODE_XTAL
+#endif
 
 // RTC clock source selection: "Internal oscillator DCO2 (32768Hz)" or "External crystal oscillator"
 #define RTC_CLOCK_SRC_DCO2 0
 #define RTC_CLOCK_SRC_EXT_XTAL 5
+#ifndef RTC_CLOCK_SRC
 #define RTC_CLOCK_SRC RTC_CLOCK_SRC_DCO2
+#endif
 
 // PCLK clock source selection: MCLK or 2xMCLK
 #define PCLK_CLOCK_SRC_MCLK 0
 #define PCLK_CLOCK_SRC_2XMCLK 1
+#ifndef PCLK_CLOCK_SRC
 #define PCLK_CLOCK_SRC PCLK_CLOCK_SRC_2XMCLK
 #endif
+
+// DCO1 calibration selection: None or External clock
+#define DCO1_CAL_SRC_NONE 0
+#define DCO1_CAL_SRC_EXT  1
+#ifndef DCO1_CAL_SRC
+#define DCO1_CAL_SRC DCO1_CAL_SRC_NONE
+#endif
+
+// Prescaler 1000 and syn preload 3000 = 16MHz external reference
+#ifndef EXT_REF_PRESCALER
+#define EXT_REF_PRESCALER 1000
+#endif
+
+#ifndef EXT_REF_SYN_PRELOAD
+#define EXT_REF_SYN_PRELOAD 3000
+#endif
+#endif
+
 
 uint32_t SystemCoreClock __attribute__((section(".no_init")));
 
@@ -99,12 +127,12 @@ void SystemCoreClockSetup(void) {
 	while((SCU_CLK->CLKCR & SCU_CLK_CLKCR_VDDC2LOW_Msk));
 #elif UC_SERIES == XMC14
 #if DCLK_CLOCK_SRC != DCLK_CLOCK_SRC_DCO1
-	if(OSCHP_GetFrequency() > 20000000U) {
+	if(OSCHP_FREQUENCY > 20000000U) {
 		SCU_ANALOG->ANAOSCHPCTRL |= SCU_ANALOG_ANAOSCHPCTRL_HYSCTRL_Msk;
 	}
 
 	// OSCHP source selection - OSC mode
-	SCU_ANALOG->ANAOSCHPCTRL = (SCU_ANALOG->ANAOSCHPCTRL & ~SCU_ANALOG_ANAOSCHPCTRL_MODE_Msk) |
+	SCU_ANALOG->ANAOSCHPCTRL = (SCU_ANALOG->ANAOSCHPCTRL & ~(SCU_ANALOG_ANAOSCHPCTRL_SHBY_Msk | SCU_ANALOG_ANAOSCHPCTRL_MODE_Msk)) |
 	                           (OSCHP_MODE << SCU_ANALOG_ANAOSCHPCTRL_MODE_Pos);
 
 	// Enable OSC_HP oscillator watchdog
@@ -123,8 +151,18 @@ void SystemCoreClockSetup(void) {
 	// check if clock is ok
 	} while(SCU_INTERRUPT->SRRAW1 & SCU_INTERRUPT_SRRAW1_LOECI_Msk);
 
+#if DCO1_CAL_SRC == DCO1_CAL_SRC_EXT
+	SCU_ANALOG->ANAOSCLPCTRL = 0;
+#endif
+
+
 	// DCLK source using OSC_HP
+#if DCO1_CAL_SRC == DCO1_CAL_SRC_EXT
+	SCU_CLK->CLKCR1 = (SCU_CLK->CLKCR1 & ~SCU_CLK_CLKCR1_DCLKSEL_Msk);
+#else
 	SCU_CLK->CLKCR1 |= SCU_CLK_CLKCR1_DCLKSEL_Msk;
+#endif
+
 #else
 	// DCLK source using DCO1
 	SCU_CLK->CLKCR1 &= ~SCU_CLK_CLKCR1_DCLKSEL_Msk;
@@ -141,11 +179,20 @@ void SystemCoreClockSetup(void) {
 	                 (RTC_CLOCK_SRC << SCU_CLK_CLKCR_RTCCLKSEL_Pos) |
 	                 (PCLK_CLOCK_SRC << SCU_CLK_CLKCR_PCLKSEL_Pos) |
 	                 0x100U; // IDIV = 1
+
+#if DCO1_CAL_SRC == DCO1_CAL_SRC_EXT
+	SCU_CLK->CLKCR = (SCU_CLK->CLKCR & (uint32_t)~(SCU_CLK_CLKCR_PCLKSEL_Msk | SCU_CLK_CLKCR_RTCCLKSEL_Msk)) | 65536;
+#endif
+
 #endif
 
 	SCU_GENERAL->PASSWD = 0x000000C3UL; // enable bit protection
 
 	SystemCoreClockUpdate();
+
+#if DCO1_CAL_SRC == DCO1_CAL_SRC_EXT
+	XMC_SCU_CLOCK_EnableDCO1ExtRefCalibration(XMC_SCU_CLOCK_SYNC_CLKSRC_OSCHP, EXT_REF_PRESCALER, EXT_REF_SYN_PRELOAD);
+#endif
 }
 
 void SystemCoreClockUpdate(void) {

6e896976fb7206b8b84ab85fa33aa95e5ef77387
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-02-03 09:41:24 +0100

    Remove unreliable us system_timer support

--- hal/system_timer/system_timer.c
+++ hal/system_timer/system_timer.c
@@ -47,30 +47,13 @@ uint32_t system_timer_get_ms(void) {
 	return system_timer_tick;
 }
 
-uint32_t system_timer_get_us(void) {
-#ifdef __XMC1__
-	return system_timer_tick*1000 + (((SystemCoreClock/1000) - SysTick->VAL)*1000)/(SystemCoreClock/1000);
-#else
-	return 0; // Implement for other MCUs here
-#endif
-}
-
 // This will work even with wrap-around up to UIN32_MAX/2 difference.
 // E.g.: end - start = 0x00000010 - 0xfffffff = 0x00000011 etc
 inline bool system_timer_is_time_elapsed_ms(const uint32_t start_measurement, const uint32_t time_to_be_elapsed) {
-	return (system_timer_get_ms() - start_measurement) >= time_to_be_elapsed;
-}
-
-inline bool system_timer_is_time_elapsed_us(const uint32_t start_measurement, const uint32_t time_to_be_elapsed) {
-	return (system_timer_get_us() - start_measurement) >= time_to_be_elapsed;
+	return (uint32_t)(system_timer_get_ms() - start_measurement) >= time_to_be_elapsed;
 }
 
 void system_timer_sleep_ms(const uint32_t sleep) {
 	const uint32_t time = system_timer_get_ms();
 	while(!system_timer_is_time_elapsed_ms(time, sleep));
-}
-
-void system_timer_sleep_us(const uint32_t sleep) {
-	const uint32_t time = system_timer_get_us();
-	while(!system_timer_is_time_elapsed_us(time, sleep));
 }

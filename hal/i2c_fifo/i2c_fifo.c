01860ad5dafd15f23f70701cdac7858d9b157b46
Olaf Lüke <olaf@tinkerforge.com>
2017-08-14 11:23:04 +0200

    Add simple XMC i2c+fifo implementation to HAL
    This implementation uses the fifo for simple interrupt-less i2c transfers.
    It assumes that the FIFO size is never exceeded.
    
    It can be used with a simple read/read+ready, write/write+ready state machine implementation.

---
+++ hal/i2c_fifo/i2c_fifo.c
@@ -0,0 +1,251 @@
+/* bricklib2
+ * Copyright (C) 2017 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * i2c_fifo.c: with fifo implementation for XMC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+**/
+
+#include "i2c_fifo.h"
+
+#include "xmc_i2c.h"
+#include "xmc_gpio.h"
+#include "xmc_usic.h"
+
+typedef enum XMC_I2C_CH_TDF {
+	XMC_I2C_CH_TDF_MASTER_SEND         = 0,
+	XMC_I2C_CH_TDF_SLAVE_SEND          = 1 << 8,
+	XMC_I2C_CH_TDF_MASTER_RECEIVE_ACK  = 2 << 8,
+	XMC_I2C_CH_TDF_MASTER_RECEIVE_NACK = 3 << 8,
+	XMC_I2C_CH_TDF_MASTER_START        = 4 << 8,
+	XMC_I2C_CH_TDF_MASTER_RESTART      = 5 << 8,
+	XMC_I2C_CH_TDF_MASTER_STOP         = 6 << 8
+} XMC_I2C_CH_TDF_t;
+
+static bool i2c_fifo_ready_or_idle(I2CFifo *i2c_fifo) {
+	return (i2c_fifo->state == I2C_FIFO_STATE_IDLE) || (i2c_fifo->state & I2C_FIFO_STATE_READY);
+}
+
+// The i2c read/write functions here assume that the FIFO size is never exceeded.
+void i2c_fifo_write_register(I2CFifo *i2c_fifo, const uint8_t reg, const uint16_t length, const uint8_t *data, const bool send_stop) {
+	if(!i2c_fifo_ready_or_idle(i2c_fifo)) {
+		i2c_fifo->state = I2C_FIFO_STATE_WRITE_REGISTER_ERROR;
+		return;
+	}
+
+	// If we don't send stop we will read directly afterwards, so the state will change to read
+	if(send_stop) {
+		i2c_fifo->state = I2C_FIFO_STATE_WRITE_REGISTER;
+	}
+
+	// I2C Master Start
+	i2c_fifo->i2c->IN[0] = (i2c_fifo->address << 1) | XMC_I2C_CH_TDF_MASTER_START;
+	// I2C Send 8 bit register
+	i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_SEND | reg;
+
+	// I2C Master send bytes to write
+	for(uint16_t i = 0; i < length; i++) {
+		i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_SEND | data[i];
+	}
+
+	if(send_stop) {
+		// I2C Master stop
+		i2c_fifo->i2c->IN[0] = (uint32_t)XMC_I2C_CH_TDF_MASTER_STOP;
+	}
+}
+
+void i2c_fifo_write_direct(I2CFifo *i2c_fifo, const uint16_t length, const uint8_t *data, const bool send_stop) {
+	if(!i2c_fifo_ready_or_idle(i2c_fifo)) {
+		i2c_fifo->state = I2C_FIFO_STATE_WRITE_DIRECT_ERROR;
+		return;
+	}
+
+	// If we don't send stop we will read directly afterwards, so the state will change to read
+	if(send_stop) {
+		i2c_fifo->state = I2C_FIFO_STATE_WRITE_DIRECT;
+	}
+
+	// I2C Master Start
+	i2c_fifo->i2c->IN[0] = (i2c_fifo->address << 1) | XMC_I2C_CH_TDF_MASTER_START;
+
+	// I2C Master send bytes to write
+	for(uint16_t i = 0; i < length; i++) {
+		i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_SEND | data[i];
+	}
+
+	if(send_stop) {
+		// I2C Master stop
+		i2c_fifo->i2c->IN[0] = (uint32_t)XMC_I2C_CH_TDF_MASTER_STOP;
+	}
+}
+
+void i2c_fifo_read_register(I2CFifo *i2c_fifo, const uint8_t reg, const uint32_t length) {
+	i2c_fifo_write_register(i2c_fifo, reg, 0, NULL, false);
+
+	if(!i2c_fifo_ready_or_idle(i2c_fifo)) {
+		i2c_fifo->state = I2C_FIFO_STATE_READ_REGISTER_ERROR;
+		return;
+	}
+
+	i2c_fifo->state = I2C_FIFO_STATE_READ_REGISTER;
+	i2c_fifo->expected_fifo_level = length;
+
+	// I2C Master restart
+	i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_RESTART | XMC_I2C_CH_CMD_READ | (i2c_fifo->address << 1);
+
+	// I2C Master send ACK/NACK
+	if(length != 0) {
+		for(uint16_t i = 0; i < length-1; i++) {
+			i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_RECEIVE_ACK;
+		}
+
+		i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_RECEIVE_NACK;
+	}
+
+	// I2C Master stop
+	i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_STOP;
+}
+
+void i2c_fifo_read_direct(I2CFifo *i2c_fifo, const uint32_t length) {
+	if(!i2c_fifo_ready_or_idle(i2c_fifo)) {
+		i2c_fifo->state = I2C_FIFO_STATE_READ_DIRECT_ERROR;
+		return;
+	}
+
+	i2c_fifo->state = I2C_FIFO_STATE_READ_DIRECT;
+	i2c_fifo->expected_fifo_level = length;
+
+	// I2C Master start
+	i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_START | XMC_I2C_CH_CMD_READ | (i2c_fifo->address << 1) ;
+
+	// I2C Master send ACK/NACK
+	if(length != 0) {
+		for(uint16_t i = 0; i < length-1; i++) {
+			i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_RECEIVE_ACK;
+		}
+
+		i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_RECEIVE_NACK;
+	}
+
+	// I2C Master stop
+	i2c_fifo->i2c->IN[0] = XMC_I2C_CH_TDF_MASTER_STOP;
+}
+
+uint8_t i2c_fifo_read_fifo(I2CFifo *i2c_fifo, uint8_t *buffer, const uint8_t buffer_length) {
+	uint8_t length = 0;
+
+	while((!XMC_USIC_CH_RXFIFO_IsEmpty(i2c_fifo->i2c)) && (length < buffer_length)) {
+		buffer[length] = XMC_I2C_CH_GetReceivedData(i2c_fifo->i2c);
+		length++;
+	}
+
+	return length;
+}
+
+void i2c_fifo_init(I2CFifo *i2c_fifo) {
+	i2c_fifo->state = I2C_FIFO_STATE_IDLE;
+
+	// First de-configure everything, so we can also use
+	// this function as i2c reset
+
+	XMC_I2C_CH_Stop(i2c_fifo->i2c);
+
+	const XMC_GPIO_CONFIG_t config_reset =  {
+		.mode             = XMC_GPIO_MODE_INPUT_PULL_UP,
+		.input_hysteresis = XMC_GPIO_INPUT_HYSTERESIS_LARGE,
+	};
+
+	XMC_GPIO_Init(i2c_fifo->sda_port, i2c_fifo->sda_pin, &config_reset);
+	XMC_GPIO_Init(i2c_fifo->scl_port, i2c_fifo->scl_pin, &config_reset);
+
+	XMC_USIC_CH_TXFIFO_Flush(i2c_fifo->i2c);
+	XMC_USIC_CH_RXFIFO_Flush(i2c_fifo->i2c);
+
+	WR_REG(i2c_fifo->i2c->FMR, USIC_CH_FMR_MTDV_Msk, USIC_CH_FMR_MTDV_Pos, 2);
+
+
+	const XMC_I2C_CH_CONFIG_t master_channel_config = {
+		.baudrate = i2c_fifo->baudrate,
+		.address  = 0
+	};
+
+	const XMC_GPIO_CONFIG_t sda_pin_config =  {
+		.mode         = i2c_fifo->sda_mode,
+		.output_level = XMC_GPIO_OUTPUT_LEVEL_HIGH,
+	};
+
+	const XMC_GPIO_CONFIG_t scl_pin_config = {
+		.mode         = i2c_fifo->scl_mode,
+		.output_level = XMC_GPIO_OUTPUT_LEVEL_HIGH,
+	};
+
+	XMC_I2C_CH_Init(i2c_fifo->i2c, &master_channel_config);
+
+	XMC_USIC_CH_SetInputSource(i2c_fifo->i2c, i2c_fifo->sda_input, i2c_fifo->sda_source);
+	XMC_USIC_CH_SetInputSource(i2c_fifo->i2c, i2c_fifo->scl_input, i2c_fifo->scl_source);
+
+	XMC_USIC_CH_TXFIFO_Configure(i2c_fifo->i2c, i2c_fifo->sda_fifo_pointer, i2c_fifo->sda_fifo_size, 0);
+	XMC_USIC_CH_RXFIFO_Configure(i2c_fifo->i2c, i2c_fifo->scl_fifo_pointer, i2c_fifo->scl_fifo_size, 0);
+
+	XMC_I2C_CH_Start(i2c_fifo->i2c);
+
+	XMC_GPIO_Init(i2c_fifo->sda_port, i2c_fifo->sda_pin, &sda_pin_config);
+	XMC_GPIO_Init(i2c_fifo->scl_port, i2c_fifo->scl_pin, &scl_pin_config);
+}
+
+I2CFifoState i2c_fifo_next_state(I2CFifo *i2c_fifo) {
+	switch(i2c_fifo->state) {
+		case I2C_FIFO_STATE_WRITE_REGISTER:
+		case I2C_FIFO_STATE_WRITE_DIRECT: {
+			const uint32_t status = XMC_I2C_CH_GetStatusFlag(i2c_fifo->i2c);
+			if(status & (XMC_I2C_CH_STATUS_FLAG_NACK_RECEIVED |
+			             XMC_I2C_CH_STATUS_FLAG_ARBITRATION_LOST |
+			             XMC_I2C_CH_STATUS_FLAG_ERROR |
+			             XMC_I2C_CH_STATUS_FLAG_WRONG_TDF_CODE_FOUND)) {
+				XMC_I2C_CH_ClearStatusFlag(i2c_fifo->i2c, 0xFFFFFFFF);
+				i2c_fifo->state |= I2C_FIFO_STATE_ERROR;
+			} else if(XMC_USIC_CH_TXFIFO_IsEmpty(i2c_fifo->i2c)) {
+				i2c_fifo->state |= I2C_FIFO_STATE_READY;
+			}
+
+			break;
+		}
+
+		case I2C_FIFO_STATE_READ_REGISTER:
+		case I2C_FIFO_STATE_READ_DIRECT: {
+			const uint32_t status = XMC_I2C_CH_GetStatusFlag(i2c_fifo->i2c);
+			if(status & (XMC_I2C_CH_STATUS_FLAG_NACK_RECEIVED |
+			             XMC_I2C_CH_STATUS_FLAG_ARBITRATION_LOST |
+			             XMC_I2C_CH_STATUS_FLAG_ERROR |
+			             XMC_I2C_CH_STATUS_FLAG_WRONG_TDF_CODE_FOUND)) {
+				XMC_I2C_CH_ClearStatusFlag(i2c_fifo->i2c, 0xFFFFFFFF);
+				i2c_fifo->expected_fifo_level = 0;
+				i2c_fifo->state |= I2C_FIFO_STATE_ERROR;
+			} else if(XMC_USIC_CH_RXFIFO_GetLevel(i2c_fifo->i2c) >= i2c_fifo->expected_fifo_level) {
+				i2c_fifo->expected_fifo_level = 0;
+				i2c_fifo->state |= I2C_FIFO_STATE_READY;
+			}
+
+			break;
+		}
+
+		default: {
+			break;
+		}
+	}
+
+	return i2c_fifo->state;
+}
---
+++ hal/i2c_fifo/i2c_fifo.h
@@ -0,0 +1,98 @@
+/* bricklib2
+ * Copyright (C) 2017 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * i2c_fifo.c: with fifo implementation for XMC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *
+ *
+ * The I2C FIFO implements simple standard i2c read/write function
+ * with the use of the fifo. This implementation assumes that
+ * the fifo is big enough to hold one complete read or write.
+ *
+ * The actual i2c transfer is done completely in hardware, there
+ * is no interrupt or similar in use.
+ *
+**/
+
+#ifndef I2C_FIFO_H
+#define I2C_FIFO_H
+
+#include "xmc_usic.h"
+#include "xmc_gpio.h"
+
+typedef enum {
+	I2C_FIFO_STATE_IDLE                 = 0,
+	I2C_FIFO_STATE_READY                = 1 << 6,
+	I2C_FIFO_STATE_ERROR                = 1 << 7,
+
+	I2C_FIFO_STATE_WRITE_REGISTER       = 1 << 0,
+	I2C_FIFO_STATE_WRITE_DIRECT         = 1 << 1,
+	I2C_FIFO_STATE_READ_REGISTER        = 1 << 2,
+	I2C_FIFO_STATE_READ_DIRECT          = 1 << 3,
+
+	I2C_FIFO_STATE_WRITE_REGISTER_READY = I2C_FIFO_STATE_WRITE_REGISTER | I2C_FIFO_STATE_READY,
+	I2C_FIFO_STATE_WRITE_REGISTER_ERROR = I2C_FIFO_STATE_WRITE_REGISTER | I2C_FIFO_STATE_ERROR,
+
+	I2C_FIFO_STATE_WRITE_DIRECT_READY   = I2C_FIFO_STATE_WRITE_DIRECT   | I2C_FIFO_STATE_READY,
+	I2C_FIFO_STATE_WRITE_DIRECT_ERROR   = I2C_FIFO_STATE_WRITE_DIRECT   | I2C_FIFO_STATE_ERROR,
+
+	I2C_FIFO_STATE_READ_REGISTER_READY  = I2C_FIFO_STATE_READ_REGISTER  | I2C_FIFO_STATE_READY,
+	I2C_FIFO_STATE_READ_REGISTER_ERROR  = I2C_FIFO_STATE_READ_REGISTER  | I2C_FIFO_STATE_ERROR,
+
+	I2C_FIFO_STATE_READ_DIRECT_READY    = I2C_FIFO_STATE_READ_DIRECT    | I2C_FIFO_STATE_READY,
+	I2C_FIFO_STATE_READ_DIRECT_ERROR    = I2C_FIFO_STATE_READ_DIRECT    | I2C_FIFO_STATE_ERROR,
+
+	I2C_FIFO_STATE_MASK                 = 0b00111111
+} I2CFifoState;
+
+typedef struct {
+	uint32_t baudrate;
+	uint8_t address;
+	XMC_USIC_CH_t *i2c;
+
+	XMC_GPIO_PORT_t *scl_port;
+	uint8_t scl_pin;
+	XMC_GPIO_MODE_t scl_mode;
+	XMC_USIC_CH_INPUT_t scl_input;
+	uint8_t scl_source;
+	XMC_USIC_CH_FIFO_SIZE_t scl_fifo_size;
+	uint8_t scl_fifo_pointer;
+
+	XMC_GPIO_PORT_t *sda_port;
+	uint8_t sda_pin;
+	XMC_GPIO_MODE_t sda_mode;
+	XMC_USIC_CH_INPUT_t sda_input;
+	uint8_t sda_source;
+	XMC_USIC_CH_FIFO_SIZE_t sda_fifo_size;
+	uint8_t sda_fifo_pointer;
+
+	uint8_t expected_fifo_level;
+	I2CFifoState state;
+} I2CFifo;
+
+
+void i2c_fifo_write_register(I2CFifo *i2c_fifo, const uint8_t reg, const uint16_t length, const uint8_t *data, const bool send_stop);
+void i2c_fifo_write_direct(I2CFifo *i2c_fifo, const uint16_t length, const uint8_t *data, const bool send_stop);
+void i2c_fifo_read_register(I2CFifo *i2c_fifo, const uint8_t reg, const uint32_t length);
+void i2c_fifo_read_direct(I2CFifo *i2c_fifo, const uint32_t length);
+uint8_t i2c_fifo_read_fifo(I2CFifo *i2c_fifo, uint8_t *buffer, const uint8_t buffer_length);
+void i2c_fifo_init(I2CFifo *i2c_fifo);
+I2CFifoState i2c_fifo_next_state(I2CFifo *i2c_fifo);
+
+
+#endif

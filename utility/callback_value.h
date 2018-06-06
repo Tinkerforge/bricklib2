f05ed9661005507ccb874d2c5387c75b80896129
Matthias Bolte <matthias@tinkerforge.com>
2018-06-06 16:50:11 +0200

    Rework callback-value handling and fix bugs
    Allow for multiple differently typed callback-values per Bricklet.
    
    Get GetCallbackValue_Response value type from config. This was hardcoded
    to int32_t before. This resulted in several Bricklets sending too long
    packets  that some API bindings (e.g. C/C++) don't handle correctly.
    
    The threshold min/max members in the packets were hardcoded to uint16_t.
    This resulted in the inability to cover the full value range for some
    Bricklets.
    
    Also rework the channel handling logic.

--- utility/callback_value.h
+++ utility/callback_value.h
@@ -1,6 +1,7 @@
 /* bricklib2
  * Copyright (C) 2017 Olaf Lüke <olaf@tinkerforge.com>
  * Copyright (C) 2018 Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
+ * Copyright (C) 2018 Matthias Bolte <matthias@tinkerforge.com>
  *
  * callback_value.h: Helper functions for Bricklet communication
  *
@@ -20,13 +21,15 @@
  * Boston, MA 02111-1307, USA.
  */
 
-#ifndef CALLBACK_VALUE
-#define CALLBACK_VALUE
+// no header guard, this file is supposed to be includable multiple times with
+// different CALLBACK_VALUE_TYPEs
 
-#include "configs/config.h"
+#include <stdint.h>
+#include <stdbool.h>
 
 #include "bricklib2/protocols/tfp/tfp.h"
 #include "bricklib2/bootloader/bootloader.h"
+#include "bricklib2/hal/system_timer/system_timer.h"
 
 #define CALLBACK_VALUE_TYPE_INT8   0
 #define CALLBACK_VALUE_TYPE_UINT8  1
@@ -35,167 +38,293 @@
 #define CALLBACK_VALUE_TYPE_INT32  4
 #define CALLBACK_VALUE_TYPE_UINT32 5
 
-#ifndef CALLBACK_VALUE_TYPE
-#define CALLBACK_VALUE_TYPE CALLBACK_VALUE_TYPE_INT16
-#endif
+#undef CALLBACK_VALUE_T
+#undef CALLBACK_VALUE_MIN
+#undef CALLBACK_VALUE_MAX
 
-#if CALLBACK_VALUE_TYPE == CALLBACK_VALUE_TYPE_INT8
-typedef int8_t callback_value_t;
+#ifndef CALLBACK_VALUE_TYPE
+#error CALLBACK_VALUE_TYPE is undefined
+#elif CALLBACK_VALUE_TYPE == CALLBACK_VALUE_TYPE_INT8
+#define CALLBACK_VALUE_T int8_t
 #define CALLBACK_VALUE_MIN INT8_MIN
 #define CALLBACK_VALUE_MAX INT8_MAX
 #elif CALLBACK_VALUE_TYPE == CALLBACK_VALUE_TYPE_UINT8
-typedef uint8_t callback_value_t;
+#define CALLBACK_VALUE_T uint8_t
 #define CALLBACK_VALUE_MIN 0
 #define CALLBACK_VALUE_MAX UINT8_MAX
 #elif CALLBACK_VALUE_TYPE == CALLBACK_VALUE_TYPE_INT16
-typedef int16_t callback_value_t;
+#define CALLBACK_VALUE_T int16_t
 #define CALLBACK_VALUE_MIN INT16_MIN
 #define CALLBACK_VALUE_MAX INT16_MAX
 #elif CALLBACK_VALUE_TYPE == CALLBACK_VALUE_TYPE_UINT16
-typedef uint16_t callback_value_t;
+#define CALLBACK_VALUE_T uint16_t
 #define CALLBACK_VALUE_MIN 0
 #define CALLBACK_VALUE_MAX UINT16_MAX
 #elif CALLBACK_VALUE_TYPE == CALLBACK_VALUE_TYPE_INT32
-typedef int32_t callback_value_t;
+#define CALLBACK_VALUE_T int32_t
 #define CALLBACK_VALUE_MIN INT32_MIN
 #define CALLBACK_VALUE_MAX INT32_MAX
 #elif CALLBACK_VALUE_TYPE == CALLBACK_VALUE_TYPE_UINT32
-typedef uint32_t callback_value_t;
+#define CALLBACK_VALUE_T uint32_t
 #define CALLBACK_VALUE_MIN 0
 #define CALLBACK_VALUE_MAX UINT32_MAX
+#else
+#error CALLBACK_VALUE_TYPE is invalid
 #endif
 
-typedef callback_value_t (*CallbackValueGetter)(void);
+// https://stackoverflow.com/questions/1489932/how-to-concatenate-twice-with-the-c-preprocessor-and-expand-a-macro-as-in-arg
+#define CALLBACK_VALUE_SUFFIX_PASTER(x, y) x ## _ ## y
+#define CALLBACK_VALUE_SUFFIX_EVALUATOR(x, y) CALLBACK_VALUE_SUFFIX_PASTER(x, y)
+#define CALLBACK_VALUE_SUFFIX(name) CALLBACK_VALUE_SUFFIX_EVALUATOR(name, CALLBACK_VALUE_T)
 
-#ifdef CHANNEL_BASED
-	typedef callback_value_t (*OnChannelCallbackValueGetter)(uint8_t channel);
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+typedef CALLBACK_VALUE_T (*CALLBACK_VALUE_SUFFIX(CallbackValueGetter))(uint8_t channel);
+#else
+typedef CALLBACK_VALUE_T (*CALLBACK_VALUE_SUFFIX(CallbackValueGetter))(void);
 #endif
 
 typedef struct {
-	CallbackValueGetter get_callback_value;
-
-	#ifdef CHANNEL_BASED
-		OnChannelCallbackValueGetter channel_based_get_callback_value;
-	#endif
-
-	callback_value_t value_last;
+	CALLBACK_VALUE_SUFFIX(CallbackValueGetter) get_callback_value;
+	CALLBACK_VALUE_T value_last;
 
 	uint32_t period;
 	uint32_t last_time;
 	bool value_has_to_change;
 
-	callback_value_t threshold_min;
-	callback_value_t threshold_max;
+	CALLBACK_VALUE_T threshold_min;
+	CALLBACK_VALUE_T threshold_max;
 	char threshold_option;
 
-	callback_value_t threshold_min_user;
-	callback_value_t threshold_max_user;
+	CALLBACK_VALUE_T threshold_min_user;
+	CALLBACK_VALUE_T threshold_max_user;
 	char threshold_option_user;
-} CallbackValue;
+} CALLBACK_VALUE_SUFFIX(CallbackValue);
 
 typedef struct {
 	TFPMessageHeader header;
-	uint32_t debounce;
-} __attribute__((__packed__)) SetDebouncePeriod;
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+	uint8_t channel;
+#endif
+} __attribute__((__packed__)) CALLBACK_VALUE_SUFFIX(GetCallbackValue);
 
 typedef struct {
 	TFPMessageHeader header;
-} __attribute__((__packed__)) GetDebouncePeriod;
+	CALLBACK_VALUE_T value;
+} __attribute__((__packed__)) CALLBACK_VALUE_SUFFIX(GetCallbackValue_Response);
 
 typedef struct {
 	TFPMessageHeader header;
-	uint32_t debounce;
-} __attribute__((__packed__)) GetDebouncePeriod_Response;
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+	uint8_t channel;
+#endif
+	uint32_t period;
+	bool value_has_to_change;
+	char option;
+	CALLBACK_VALUE_T min;
+	CALLBACK_VALUE_T max;
+} __attribute__((__packed__)) CALLBACK_VALUE_SUFFIX(SetCallbackValueCallbackConfiguration);
 
 typedef struct {
 	TFPMessageHeader header;
-} __attribute__((__packed__)) GetCallbackValue;
-
-typedef struct {
-	TFPMessageHeader header;
-	int32_t value;
-} __attribute__((__packed__)) GetCallbackValue_Response;
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+	uint8_t channel;
+#endif
+} __attribute__((__packed__)) CALLBACK_VALUE_SUFFIX(GetCallbackValueCallbackConfiguration);
 
 typedef struct {
 	TFPMessageHeader header;
 	uint32_t period;
 	bool value_has_to_change;
 	char option;
-	uint16_t min;
-	uint16_t max;
-} __attribute__((__packed__)) SetCallbackValueCallbackConfiguration;
+	CALLBACK_VALUE_T min;
+	CALLBACK_VALUE_T max;
+} __attribute__((__packed__)) CALLBACK_VALUE_SUFFIX(GetCallbackValueCallbackConfiguration_Response);
 
 typedef struct {
 	TFPMessageHeader header;
-} __attribute__((__packed__)) GetCallbackValueCallbackConfiguration;
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+	uint8_t channel;
+#endif
+	CALLBACK_VALUE_T value;
+} __attribute__((__packed__)) CALLBACK_VALUE_SUFFIX(CallbackValue_Callback);
 
-typedef struct {
-	TFPMessageHeader header;
-	uint32_t period;
-	bool value_has_to_change;
-	char option;
-	uint16_t min;
-	uint16_t max;
-} __attribute__((__packed__)) GetCallbackValueCallbackConfiguration_Response;
-
-typedef struct {
-	TFPMessageHeader header;
-	callback_value_t value;
-} __attribute__((__packed__)) CallbackValue_Callback;
-
-void callback_value_init(CallbackValue *callback_value, CallbackValueGetter callback_value_getter);
-
-BootloaderHandleMessageResponse set_debounce_period(const SetDebouncePeriod *data);
-BootloaderHandleMessageResponse get_debounce_period(const GetDebouncePeriod *data, GetDebouncePeriod_Response *response);
-
-BootloaderHandleMessageResponse get_callback_value(const GetCallbackValue *data, GetCallbackValue_Response *response, CallbackValue *callback_value);
-BootloaderHandleMessageResponse set_callback_value_callback_configuration(const SetCallbackValueCallbackConfiguration *data, CallbackValue *callback_value);
-BootloaderHandleMessageResponse get_callback_value_callback_configuration(const GetCallbackValueCallbackConfiguration *data, GetCallbackValueCallbackConfiguration_Response *response, CallbackValue *callback_value);
-
-bool handle_callback_value_callback(CallbackValue *callback_value, const uint8_t fid);
-
-#ifdef CHANNEL_BASED
-	typedef struct {
-		TFPMessageHeader header;
-		uint8_t channel;
-	} __attribute__((__packed__)) OnChannelGetCallbackValue;
-
-	typedef struct {
-		TFPMessageHeader header;
-		uint32_t period;
-		bool value_has_to_change;
-		char option;
-		uint16_t min;
-		uint16_t max;
-		uint8_t channel;
-	} __attribute__((__packed__)) OnChannelSetCallbackValueCallbackConfiguration;
-
-	typedef struct {
-		TFPMessageHeader header;
-		uint8_t channel;
-	} __attribute__((__packed__)) OnChannelGetCallbackValueCallbackConfiguration;
-
-	typedef struct {
-		TFPMessageHeader header;
-		callback_value_t value;
-		uint8_t channel;
-	} __attribute__((__packed__)) OnChannelCallbackValue_Callback;
-
-	void channel_based_callback_value_init(CallbackValue *callback_value,
-	                                       OnChannelCallbackValueGetter callback_value_getter);
-
-	BootloaderHandleMessageResponse channel_based_get_callback_value(const OnChannelGetCallbackValue *data,
-	                                                                 GetCallbackValue_Response *response,
-	                                                                 CallbackValue *callback_value);
-
-	BootloaderHandleMessageResponse channel_based_set_callback_value_callback_configuration(const OnChannelSetCallbackValueCallbackConfiguration *data,
-	                                                                                        CallbackValue *callback_value);
-
-	BootloaderHandleMessageResponse channel_based_get_callback_value_callback_configuration(const OnChannelGetCallbackValueCallbackConfiguration *data,
-	                                                                                        GetCallbackValueCallbackConfiguration_Response *response,
-	                                                                                        CallbackValue *callback_value);
-
-	bool channel_based_handle_callback_value_callback(CallbackValue *callback_value, const uint8_t fid, const uint8_t channel);
+void CALLBACK_VALUE_SUFFIX(callback_value_init)(CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_values,
+                                                CALLBACK_VALUE_SUFFIX(CallbackValueGetter) callback_value_getter) {
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+	for (uint8_t channel = 0; channel < CALLBACK_VALUE_CHANNEL_NUM; ++channel) {
+		CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_value = &callback_values[channel];
+#else
+		CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_value = callback_values;
 #endif
 
+		callback_value->get_callback_value    = callback_value_getter;
+		callback_value->value_last            = CALLBACK_VALUE_MAX;
+
+		callback_value->period                = 0;
+		callback_value->last_time             = 0;
+		callback_value->value_has_to_change   = false;
+
+		callback_value->threshold_min         = 0;
+		callback_value->threshold_max         = 0;
+		callback_value->threshold_option      = 'x';
+
+		callback_value->threshold_min_user    = 0;
+		callback_value->threshold_max_user    = 0;
+		callback_value->threshold_option_user = 'x';
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+	}
 #endif
+}
+
+BootloaderHandleMessageResponse
+CALLBACK_VALUE_SUFFIX(get_callback_value)(const CALLBACK_VALUE_SUFFIX(GetCallbackValue) *data,
+                                          CALLBACK_VALUE_SUFFIX(GetCallbackValue_Response) *response,
+                                          CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_values) {
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+	if (data->channel >= CALLBACK_VALUE_CHANNEL_NUM) {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	const CALLBACK_VALUE_T value_current = callback_values[data->channel].get_callback_value(data->channel);
+#else
+	const CALLBACK_VALUE_T value_current = callback_values->get_callback_value();
+#endif
+
+	response->header.length = sizeof(CALLBACK_VALUE_SUFFIX(GetCallbackValue_Response));
+	response->value         = value_current;
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse
+CALLBACK_VALUE_SUFFIX(set_callback_value_callback_configuration)(const CALLBACK_VALUE_SUFFIX(SetCallbackValueCallbackConfiguration) *data,
+                                                                 CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_values) {
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+	if (data->channel >= CALLBACK_VALUE_CHANNEL_NUM) {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_value = &callback_values[data->channel];
+#else
+	CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_value = callback_values;
+#endif
+
+	if(data->option == 'o' || data->option == 'i' || data->option == 'x') {
+		callback_value->threshold_option = data->option;
+		callback_value->threshold_min = data->min;
+		callback_value->threshold_max = data->max;
+	} else if(data->option == '<') {
+		callback_value->threshold_option = 'o';
+		callback_value->threshold_min = data->min;
+		callback_value->threshold_max = CALLBACK_VALUE_MAX;
+	} else if(data->option == '>') {
+		callback_value->threshold_option = 'o';
+		callback_value->threshold_min = CALLBACK_VALUE_MIN;
+		callback_value->threshold_max = data->min;
+	} else {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	callback_value->threshold_option_user = data->option;
+	callback_value->threshold_min_user = data->min;
+	callback_value->threshold_max_user = data->max;
+
+	callback_value->period = data->period;
+	callback_value->value_has_to_change = data->value_has_to_change;
+
+	// We make sure to always send the first piece of data
+	// immediately after a new callback configuration
+	callback_value->last_time = system_timer_get_ms() - data->period;
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse
+CALLBACK_VALUE_SUFFIX(get_callback_value_callback_configuration)(const CALLBACK_VALUE_SUFFIX(GetCallbackValueCallbackConfiguration) *data,
+                                                                 CALLBACK_VALUE_SUFFIX(GetCallbackValueCallbackConfiguration_Response) *response,
+                                                                 CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_values) {
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+	if (data->channel >= CALLBACK_VALUE_CHANNEL_NUM) {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_value = &callback_values[data->channel];
+#else
+	CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_value = callback_values;
+#endif
+
+	response->header.length       = sizeof(CALLBACK_VALUE_SUFFIX(GetCallbackValueCallbackConfiguration_Response));
+	response->period              = callback_value->period;
+	response->value_has_to_change = callback_value->value_has_to_change;
+	response->option              = callback_value->threshold_option_user;
+	response->min                 = callback_value->threshold_min_user;
+	response->max                 = callback_value->threshold_min_user;
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+bool CALLBACK_VALUE_SUFFIX(handle_callback_value_callback)(CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_values,
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+                                                           const uint8_t channel,
+#endif
+                                                           const uint8_t fid) {
+	static bool is_buffered = false;
+	static CALLBACK_VALUE_SUFFIX(CallbackValue_Callback) cb;
+
+	if(!is_buffered) {
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+		CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_value = &callback_values[channel];
+		const CALLBACK_VALUE_T value_current = callback_values->get_callback_value(channel);
+#else
+		CALLBACK_VALUE_SUFFIX(CallbackValue) *callback_value = callback_values;
+		const CALLBACK_VALUE_T value_current = callback_values->get_callback_value();
+#endif
+
+		tfp_make_default_header(&cb.header, bootloader_get_uid(), sizeof(CALLBACK_VALUE_SUFFIX(CallbackValue_Callback)), fid);
+
+		// If period is 0 callback is off
+		// If period > 0 and not elapsed we don't send
+		if((callback_value->period == 0) ||
+		   (!system_timer_is_time_elapsed_ms(callback_value->last_time, callback_value->period))) {
+			return false;
+		}
+
+		// If value has to change and current value is equal to last value we don't send
+		if(callback_value->value_has_to_change &&
+		   (value_current == callback_value->value_last)) {
+			return false;
+		}
+
+		// If outside-threshold is defined but not fulfilled we son't send
+		if((callback_value->threshold_option == 'o') &&
+		   ((value_current >= callback_value->threshold_min) &&
+		    (value_current <= callback_value->threshold_max))) {
+			return false;
+		}
+
+		// If inside-threshold is defined but not fulfilled we son't send
+		if((callback_value->threshold_option == 'i') &&
+		   ((value_current < callback_value->threshold_min) ||
+		    (value_current > callback_value->threshold_max))) {
+			return false;
+		}
+
+		// If none of the above returned, we know that a callback is triggered
+		callback_value->last_time  = system_timer_get_ms();
+		callback_value->value_last = value_current;
+#ifdef CALLBACK_VALUE_CHANNEL_NUM
+		cb.channel                 = channel;
+#endif
+		cb.value                   = value_current;
+	}
+
+	if(bootloader_spitfp_is_send_possible(&bootloader_status.st)) {
+		bootloader_spitfp_send_ack_and_message(&bootloader_status, (uint8_t*)&cb, sizeof(CALLBACK_VALUE_SUFFIX(CallbackValue_Callback)));
+		is_buffered = false;
+		return true;
+	} else {
+		is_buffered = true;
+	}
+
+	return false;
+}
